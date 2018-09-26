f29d4369a68c7b62907f9db4987e6e545a8e532d
unknown <qinziang@hujiang.com>
2018-09-26 18:32:58 +0800

    set level_1 basic example sentences.

--- level_1.py
+++ level_1.py
@@ -1,9 +1,38 @@
 from nltk_parser import sentence2tree
 from nltk_parser import draw_tree
 
-sentArr_1 = {
-    "Birds sing beautifully",
-    "He went on holiday",
-    "We stopped to have a rest.",
-    "I'll go swimming."
-}
+sentArr = [
+    [
+        "Birds sing beautifully",
+        "He went on holiday",
+        "We stopped to have a rest.",
+        "I'll go swimming."
+    ],
+    [
+        "He is a boy.",
+        "She is beautiful.",
+        "Class isover.",
+        "He isin good health.",
+        "He is excited. "
+     ],
+    [
+        "I like music.", "She hates him.",
+        "I want to help him.",
+        "I don't know what to do.",
+        "I enjoy living here.",
+        "I don't think he is right.", "I don't think that he is right."
+    ],
+    [
+        "She gave me a new book.", "I sent him a book.", "Give me a cup of tea ,please."
+        "He sent a book to me.", " Show this house to Mr. Smith."
+    ],
+    [
+        "We named our babyTom.",
+        "He painted the wall white.",
+        "I often find him at work.",
+        "I wish you to stay.",
+        "I heard my name called.",
+        "I feel something moving.",
+        "He show me how to do it."
+    ],
+]
